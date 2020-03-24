f604008f064969180efc7e10b09edc83d261b43a
Alfian Losari <alfianlosari@gmail.com>
2020-03-24 09:43:37 +0800

    Initial Commit

---
+++ SwiftUIAuthentication/Views/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUIAuthentication
+//
+//  Created by Alfian Losari on 22/03/20.
+//  Copyright © 2020 Alfian Losari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+         
+     var body: some View {
+         Text("Hello SwiftUI + Firebase Auth!")
+     }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
